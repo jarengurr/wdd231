bacc7f2adf31581a058bc1a939caf1306ecb1187
jarengurr <jarenwheat@gmail.com>
2026-03-10 15:42:16 -0700

    update

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,11 +1,7 @@
 const navButton = document.querySelector("#ham-bt");
-navButton.addEventListener('click', () => {
-    navButton.classList.toggle('show');
-});
-
 const navBar = document.querySelector('#nav-list');
-
-navBar.addEventListener('click', () => {
-  navBar.classList.toggle('show'); 
-  navBar.classList.toggle('show');  
+navButton.addEventListener('click', () => {
+  navButton.classList.toggle('show');
+  navBar.classList.toggle('show');
 });
+
