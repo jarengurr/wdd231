c836557daea6af1ee39df938db008a6e3b6d1742
jarengurr <jarenwheat@gmail.com>
2026-03-13 22:39:50 -0700

    update

---
+++ scripts/course.js
@@ -0,0 +1,28 @@
+const classes = [
+    {
+        className: "CSE110",
+        completed: "YES"
+       
+       
+    },
+    {
+        className: "WDD130",
+        completed: "YES"
+    },
+    {
+        className: "WDD131",
+        completed: "YES"
+    },
+     {
+        className: "WDD231",
+        completed: "NO"
+    },
+      {
+        className: "CSE111",
+        completed: "YES"
+    },
+    ];
+
+
+
+const navButton = document.querySelector("#ham-bt");
--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,7 +1,9 @@
 const navButton = document.querySelector("#ham-bt");
 const navBar = document.querySelector('#nav-list');
-navButton.addEventListener('click', () => {
-  navButton.classList.toggle('show');
-  navBar.classList.toggle('show');
+
+navButton.addEventListener("click", () => {
+ 
+  navBar.classList.toggle("show");
+  navButton.classList.toggle("show");
 });
 
