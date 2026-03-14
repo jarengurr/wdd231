84b085bf6031d3d4e21530f51e83c4f41d7c78a2
jarengurr <jarenwheat@gmail.com>
2026-03-14 13:04:45 -0700

    update

--- scripts/course.js
+++ scripts/course.js
@@ -1,27 +1,67 @@
 const classes = [
     {
         className: "CSE110",
+        classType: "cse",
+        credit: 2,
         completed: "YES"
-       
-       
+    },
+    {
+        className: "CSE111",
+        classType: "cse",
+        credit: 2,
+        completed: "YES"
     },
     {
         className: "WDD130",
+        classType: "wdd",
+        credit: 2,
         completed: "YES"
     },
     {
         className: "WDD131",
+        classType: "wdd",
+        credit: 2,
         completed: "YES"
     },
-     {
+    {
         className: "WDD231",
+        classType: "wdd",
+        credit: 2,
         completed: "NO"
     },
-      {
-        className: "CSE111",
-        completed: "YES"
-    },
+      
     ];
+
+
+const myClases = classes;
+
+function showAll() {
+     
+   myClases = myClases;
+    createTempleCard();
+}
+
+
+function showWdd() {
+    // let namesB = names.filter(name => name.charAt(0) === 'B');
+    
+    //document.querySelector(".piclist").removeChild();
+    
+    mytemples = mytemples.filter(temple => temple.area > 90000);
+    createTempleCard();
+}
+function showCse() {
+    
+    
+    mytemples = mytemples.filter(temple => temple.area > 90000);
+    createTempleCard();
+}
+function findCredits() {
+       
+    mytemples = mytemples.filter(temple => temple.area > 90000);
+    createTempleCard();
+}
+
 
 
 
