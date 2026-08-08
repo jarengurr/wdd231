759113f7d7bec40795df34e72d4e0a2c7b7ec54c
jarengurr <jarenwheat@gmail.com>
2026-08-08 14:04:30 -0700

    one

--- chamber/scripts/timeday.js
+++ chamber/scripts/timeday.js
@@ -1,27 +1,71 @@
 const todaydate = document.querySelector("today");  
 const numStored = localStorage.length; 
 const lessday = "Back so soon! Awesome!";
-const moreday = "You last visited n days ago.";
-const firstday = "Welcome! Let us know if you have any questions."
-//if
+const msday = 86400000;
+
+const firstday = "Welcome! Let us know if you have any questions.";
 
 
 
-localStorage.clear(); 
+function clearStorage() {
+  localStorage.clear(); 
+}
 
-for (i = 0; i < localStorage.length; i++) {
+function hold() {
+  for (i = 0; i < localStorage.length; i++) {
   x = localStorage.key(i);
-  document.getElementById("demo").innerHTML += x;
+  document.getElementById("greeting").innerHTML += x;
 } 
 
-function createItems() {
-  localStorage.setItem("mytime", Date.now());
-  localStorage.setItem("myname", "John");
-    localStorage.setItem("myage", 36);
-    
 }
-createItems();
+
+
+
 
 function storeTime() {
   localStorage.setItem("visit", Date.now());
 }
+
+window.addEventListener("load", function () {
+    let numHold = localStorage.length;
+    let msamount = localStorage.key(numHold);
+
+  if (localStorage.length === 0) {
+    document.getElementById("greeting").innerHTML =  firstday;
+  }
+  if (msamount / msday < 1) {
+      document.getElementById("greeting").innerHTML = lessday;
+    }
+  
+  if (localStorage.length >= 1) {
+    
+    
+    
+    
+    if (Math.round(msamount / msday) === 1) {
+      document.getElementById("greeting").innerHTML =  "You last visited 1 day ago.";
+    }
+
+    if (msamount / msday > 1) {
+
+      document.getElementById("greeting").innerHTML =  `You last visited ${Math.round(msamount / msday)} days ago.`;
+    }
+
+  }
+
+  storeTime();
+  //clearStorage();
+  //document.getElementById("greeting").innerHTML = localStorage.key(1);
+  //localStorage.removeItem(localStorage.key(1)); 
+  //document.getElementById("greeting").innerHTML = localStorage.length;
+});
+    
+  
+
+
+
+
+
+
+
+  
